2c9579fa823d44969b4b830e779cf767861a3e52
Pallavi <97425707+Pallavi-kemburu@users.noreply.github.com>
2023-02-01 11:59:38 +0530

    import java.util.*; class A{     public static void main(String args[])     {         int a,b,c;         Scanner s=new Scanner(System.in);
                a=s.nextInt();
                b=s.nextInt();
                c=s.nextInt();
                if (a>50 && b>60 && c>100)
                {
                    System.out.println("10");
                }
                else if(a>50 && b>60 && c<=100)
                {
                    System.out.println("9");
                }
                else if(a<=50 && b>60 && c>100)
                {
                    System.out.println("8");
                }
                else if(a>50 && b<=60 && c>100)
                {
                    System.out.println("7");
                }
                else if(a>50 || b>60 || c>100)
                {
                    System.out.println("6");
                }
                else 
                {
                    System.out.println("5");
                }
            
        }
    }

---
+++ Grades_of_Rides.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class A{
+    public static void main(String args[])
+    {
+        int a,b,c;
+        Scanner s=new Scanner(System.in);
+        
+            a=s.nextInt();
+            b=s.nextInt();
+            c=s.nextInt();
+            if (a>50 && b>60 && c>100)
+            {
+                System.out.println("10");
+            }
+            else if(a>50 && b>60 && c<=100)
+            {
+                System.out.println("9");
+            }
+            else if(a<=50 && b>60 && c>100)
+            {
+                System.out.println("8");
+            }
+            else if(a>50 && b<=60 && c>100)
+            {
+                System.out.println("7");
+            }
+            else if(a>50 || b>60 || c>100)
+            {
+                System.out.println("6");
+            }
+            else 
+            {
+                System.out.println("5");
+            }
+        
+    }
+}
