84ad74d07fecae3618a2a418476b124bbc2677f3
Pallavi <97425707+Pallavi-kemburu@users.noreply.github.com>
2023-02-01 11:28:57 +0530

    import java.util.*; class A{     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int t,se,b;        // long long int r;         t=s.nextInt();         se=s.nextInt();         b=s.nextInt();
            System.out.println(2*t*se*b*512);
        }
    }

---
+++ Disk_Capacity.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class A{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int t,se,b;
+       // long long int r;
+        t=s.nextInt();
+        se=s.nextInt();
+        b=s.nextInt();
+    
+        System.out.println(2*t*se*b*512);
+    }
+}
